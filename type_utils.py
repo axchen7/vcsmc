1e263851c18fc2e9b615b63a08812fbcd43ab833
Alex Chen <axchen7@outlook.com>
2024-02-14 15:48:21 -0500

    Refactor tf_function decorator in type_utils.py

--- type_utils.py
+++ type_utils.py
@@ -2,18 +2,15 @@ from typing import Any, Callable, TypeVar
 
 import tensorflow as tf
 
+T = TypeVar("T", bound=Callable)
 
-def tf_function(*args, **kwargs):
+
+def tf_function(*args, **kwargs) -> Callable[[T], T]:
     """
     Like @tf.function, but without masking the original function's type signature.
     """
 
-    T = TypeVar("T", bound=Callable)
-
-    def decorator(func: T) -> T:
-        return tf.function(func, *args, **kwargs)  # type: ignore
-
-    return decorator
+    return tf.function(*args, **kwargs)  # type: ignore
 
 
 Tensor = Any
