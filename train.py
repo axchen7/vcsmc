6c5d083c1b1c9ff36cf12d1aafeb3a15cf4df86b
Alex Chen <axchen7@outlook.com>
2024-02-14 04:38:16 -0500

    Add train function for VCSMC model

---
+++ train.py
@@ -0,0 +1,128 @@
+from io import StringIO
+
+import keras
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from Bio import Phylo
+from tqdm import tqdm
+
+import utils
+import vcsmc
+from type_utils import Tensor, tf_function
+
+
+def train(
+    vcsmc: vcsmc.VCSMC,
+    optimizer: keras.optimizers.Optimizer,
+    data_NxSxA: Tensor,
+    taxa: list[str],
+    root: str = "Healthy",
+    *,
+    epochs: int,
+    stat_probs_reg_lambda: float = 0,
+):
+    @tf_function()
+    def train_step(batch_NxSxA: Tensor):
+        with tf.GradientTape() as tape:
+            log_Z_SMC, log_likelihoods_K = vcsmc(batch_NxSxA)
+
+            cost = -log_Z_SMC
+
+            # encourage uniform stationary probabilities
+            stat_probs = vcsmc.markov.stat_probs()
+            stat_probs_reg = tf.math.reduce_sum(tf.math.square(stat_probs))
+
+            # tally up regularization cost
+            total_reg = stat_probs_reg * stat_probs_reg_lambda
+
+            # weight by batch size
+            total_reg *= batch_NxSxA.shape[0]
+
+            cost += total_reg
+
+        grads = tape.gradient(cost, vcsmc.trainable_variables)
+        optimizer.apply_gradients(zip(grads, vcsmc.trainable_variables))  # type: ignore
+
+        return log_Z_SMC, log_likelihoods_K
+
+    N = data_NxSxA.shape[0]
+
+    results_dir = utils.create_results_dir()
+    summary_writer = tf.summary.create_file_writer(results_dir)  # type: ignore
+
+    log_likelihoods_across_epochs = []
+    avg_log_likelihoods_across_epochs = []
+
+    for epoch in tqdm(range(epochs)):
+        log_Z_SMC, log_likelihoods_K = train_step(data_NxSxA)
+
+        log_likelihoods_avg = tf.math.reduce_mean(log_likelihoods_K)
+        log_likelihoods_std_dev = tf.math.reduce_std(log_likelihoods_K)
+        # best_tree = newick_trees[tf.math.argmax(log_likelihoods_K)]
+
+        avg_log_likelihoods_across_epochs.append(log_likelihoods_avg)
+        log_likelihoods_across_epochs.append(log_likelihoods_K)
+
+        with summary_writer.as_default(step=epoch):
+            tf.summary.scalar("Log likelihood avg", log_likelihoods_avg)
+            tf.summary.scalar("Log likelihoods std dev", log_likelihoods_std_dev)
+            tf.summary.histogram("Log likelihoods", log_likelihoods_K)
+
+            if (epoch + 1) % 4 == 0:
+                # # ===== leaf embeddings =====
+
+                # leaf_embeddings = evsmc.leaf_embeddings(data_SxNxA)
+                # leaf_embeddings = evsmc.distance.project_many(leaf_embeddings)
+
+                # plt.scatter(leaf_embeddings[:, 0], leaf_embeddings[:, 1])
+
+                # for i, taxon in enumerate(taxa):
+                #     plt.annotate(
+                #         taxon,
+                #         (leaf_embeddings[i, 0], leaf_embeddings[i, 1]),
+                #         fontsize="xx-small",
+                #     )
+
+                # tf.summary.image("Leaf embeddings", utils.cur_plt_to_tf_image())
+
+                # # ===== best tree =====
+
+                # _, axes = plt.subplots(figsize=(10, N * 0.2))
+
+                # phylo_tree = Phylo.read(StringIO(best_tree.numpy().decode()), "newick")
+                # phylo_tree.root_with_outgroup(root)
+                # Phylo.draw(phylo_tree, axes=axes, do_show=False)
+
+                # tf.summary.image("Best tree", utils.cur_plt_to_tf_image())
+
+                # ===== Q matrix =====
+
+                Q = vcsmc.markov.Q()
+                plt.imshow(Q)
+
+                tf.summary.image("Q matrix", utils.cur_plt_to_tf_image())
+
+    # ===== done training! =====
+
+    print("Training complete!")
+
+    # ===== final log likelihoods across epochs =====
+
+    K = log_likelihoods_across_epochs[0].shape[0]
+
+    plt.scatter(
+        np.arange(epochs).repeat(K),
+        np.array(log_likelihoods_across_epochs).flatten(),
+        c="black",
+        alpha=0.2,
+    )
+    plt.plot(
+        np.arange(epochs),
+        avg_log_likelihoods_across_epochs,
+        c="orange",
+    )
+    plt.xlabel("Epoch")
+    plt.ylabel("Log Likelihood")
+    plt.title("Log Likelihoods Across Epochs")
+    plt.show()
