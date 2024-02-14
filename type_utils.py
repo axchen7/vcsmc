46db89db312ab3b61a70d5f029d53c345fdff4f8
Alex Chen <axchen7@outlook.com>
2024-02-14 04:43:41 -0500

    Add comment

--- type_utils.py
+++ type_utils.py
@@ -4,9 +4,14 @@ import tensorflow as tf
 
 
 def tf_function(*args, **kwargs):
+    """
+    Like @tf.function, but without masking the original function's type signature.
+    """
+
     T = TypeVar("T", bound=Callable)
 
     def decorator(func: T) -> T:
+        return func
         return tf.function(func, *args, **kwargs)  # type: ignore
 
     return decorator
