713a4b1690051bdc35941071ad20267ae0235766
Alex Chen <axchen7@outlook.com>
2024-04-07 17:31:47 -0400

    Add find_most_recent_file

--- train_utils.py
+++ train_utils.py
@@ -1,3 +1,6 @@
+import glob
+import os
+
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LambdaLR
 
@@ -22,3 +25,22 @@ class slow_start_lr_scheduler(LambdaLR):
 
     def lr_lambda(self, epoch: int):
         return self.scale if epoch < self.cutoff else 1.0
+
+
+def find_most_recent_file(dir: str, filename: str) -> str:
+    """
+    Args:
+        dir: The path to the directory to recursively search.
+        filename: The name of the file to search for.
+
+    Returns:
+        The path of the most recently created file with the given name.
+    """
+    file_list = glob.glob(f"{dir}/**/{filename}", recursive=True)
+
+    if len(file_list) == 0:
+        raise FileNotFoundError(
+            f'No files found with the name "{filename}" in directory "{dir}".'
+        )
+
+    return max(file_list, key=os.path.getctime)
