cd468627f19d5e63c9ea20e77fb173b7c3aa2ccc
Alex Chen <axchen7@outlook.com>
2024-04-29 13:55:30 +0000

    Flush bm

--- figures/hyp_smc_benchmark.py
+++ figures/hyp_smc_benchmark.py
@@ -135,9 +135,11 @@ files = [f"data/hohna/DS{i}.phy" for i in range(1, 8)]
 
 with open(output_file, "w") as f:
     f.write("file,ll_mean,ll_std_dev\n")
+    f.flush()
 
     for file in files:
         ll_mean, ll_std_dev = estimate_log_likelihood(file)
         f.write(f"{file},{ll_mean:.2f},{ll_std_dev:.2f}\n")
+        f.flush()
 
 # %%
