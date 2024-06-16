09db89a642ca8217753cf738c250932bfa597455
Alex Chen <azc2110@columbia.edu>
2024-06-16 09:29:46 +0000

    Add train_driver.py

---
+++ scripts/train_driver.py
@@ -0,0 +1,57 @@
+from argparse import ArgumentParser
+
+from vcsmc import *
+
+
+def main():
+    parser = ArgumentParser(description="Train a VCSMC model on a phylogenetic dataset")
+    parser.add_argument("file", type=str)
+    parser.add_argument("--K", type=int, required=True)
+    parser.add_argument("--D", type=int, required=True)
+    parser.add_argument("--lr", type=float, required=True)
+    parser.add_argument("--epochs", type=int, required=True)
+    parser.add_argument("--sites-batch-size", type=int, required=False)
+    parser.add_argument("--lookahead-merge", action="store_true")
+    parser.add_argument("--hash-trick", action="store_true")
+    parser.add_argument("--checkpoint-grads", action="store_true")
+    args = parser.parse_args()
+
+    device = detect_device()
+
+    file = "data/hohna/DS1.phy"
+
+    N, _S, A, data_NxSxA, taxa_N = load_phy(file, A4_ALPHABET)
+    data_NxSxA = data_NxSxA.to(device)
+
+    distance = Hyperbolic()
+    seq_encoder = EmbeddingTableSequenceEncoder(distance, data_NxSxA, D=args.D)
+    merge_encoder = HyperbolicGeodesicMidpointMergeEncoder(distance)
+
+    proposal = EmbeddingProposal(
+        distance, seq_encoder, merge_encoder, N=N, lookahead_merge=args.lookahead_merge
+    )
+
+    q_matrix_decoder = JC69QMatrixDecoder(A=A)
+    vcsmc = VCSMC(
+        q_matrix_decoder,
+        proposal,
+        N=N,
+        K=args.K,
+        hash_trick=args.hash_trick,
+        checkpoint_grads=args.checkpoint_grads,
+    ).to(device)
+    optimizer = torch.optim.Adam(vcsmc.parameters(), lr=args.lr)
+
+    train(
+        vcsmc,
+        optimizer,
+        taxa_N,
+        data_NxSxA,
+        file,
+        epochs=args.epochs,
+        sites_batch_size=args.sites_batch_size,
+    )
+
+
+if __name__ == "__main__":
+    main()
