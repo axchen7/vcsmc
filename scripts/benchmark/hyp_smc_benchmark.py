0888bd1817e17b24986eb7eebf2d131321bdbbbb
Alex Chen <axchen7@outlook.com>
2024-07-07 10:06:40 +0000

    Fix output

--- scripts/benchmark/hyp_smc_benchmark.py
+++ scripts/benchmark/hyp_smc_benchmark.py
@@ -1,5 +1,6 @@
 import csv
 import os
+from datetime import datetime
 
 from scripts.train.hyp_train_hybrid import hyp_train_hybrid
 from scripts.utils.estimate_latest_run_ll import estimate_latest_run_ll
@@ -8,7 +9,6 @@ from scripts.utils.estimate_latest_run_ll import estimate_latest_run_ll
 DATASETS = ["DS1", "DS2", "DS3", "DS4", "DS5", "DS6", "DS8"]
 
 ESTIMATE_LL_SAMPLES = 10
-OUTPUT_FILE = "outputs/benchmark/hyp_smc_benchmark.csv"
 
 
 def train(dataset: str):
@@ -32,13 +32,16 @@ def train(dataset: str):
 
 
 def hyp_smc_benchmark():
-    os.makedirs(os.path.dirname(OUTPUT_FILE), exist_ok=True)
+    date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    output_file = f"outputs/benchmark/hyp_smc_benchmark_{date}.csv"
 
-    with open(OUTPUT_FILE, "w", newline="") as f:
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+    with open(output_file, "w", newline="") as f:
         writer = csv.writer(f)
 
         # write csv headers
-        writer.writerows(["dataset", "LL mean", "LL std dev"])
+        writer.writerow(["dataset", "LL mean", "LL std dev"])
         f.flush()
 
         for dataset in DATASETS:
