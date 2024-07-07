a7c4ee097ef06bf5b2f4935bd1fd143d647b0f8a
Alex Chen <axchen7@outlook.com>
2024-07-07 10:07:36 +0000

    No grad

--- scripts/utils/estimate_latest_run_ll.py
+++ scripts/utils/estimate_latest_run_ll.py
@@ -1,9 +1,11 @@
 import numpy as np
+import torch
 from tqdm import tqdm
 
 from vcsmc import evaluate, load_checkpoint
 
 
+@torch.no_grad()
 def estimate_latest_run_ll(num_samples: int) -> tuple[float, float]:
     """
     Sampling a few log ZCSMCs from the best epoch of the latest run.
