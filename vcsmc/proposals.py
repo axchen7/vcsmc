0f5a177c3ce6e5ea6a903ee81502f8f1384c9467
Alex Chen <axchen7@outlook.com>
2024-09-09 02:58:52 +0000

    Move all manifold to CPU

--- vcsmc/proposals.py
+++ vcsmc/proposals.py
@@ -402,11 +402,22 @@ class EmbeddingProposal(Proposal):
                 embedding_normalized_D = self.distance.normalize(
                     embedding_D.unsqueeze(0)
                 ).squeeze(0)
+
+                # manifold operations only work on CPU
+
+                # move inputs to CPU
+                embedding_normalized_D = embedding_normalized_D.to("cpu")
+                samples_D = samples_D.to("cpu")
+
+                # do manifold operations
                 transported_D = manifold.transp0(embedding_normalized_D, samples_D)
-                # expmap only works on CPU
                 embedding_normalized_D = manifold.expmap(
-                    embedding_normalized_D.to("cpu"), transported_D.to("cpu")
-                ).to(device)
+                    embedding_normalized_D, transported_D
+                )
+
+                # move outputs back to device
+                embedding_normalized_D = embedding_normalized_D.to(device)
+
                 embedding_D = self.distance.unnormalize(
                     embedding_normalized_D.unsqueeze(0)
                 ).squeeze(0)
