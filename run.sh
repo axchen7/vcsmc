087c490538d07d043eedb092085aac7f6b3d7615
Alex Chen <axchen7@outlook.com>
2024-10-07 18:04:12 -0400

    Remove stuff

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
-python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
-python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
-python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
-python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
