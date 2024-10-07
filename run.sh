b77edaf0dd221a53e267df0f758ef0aa43b04beb
Alex Chen <axchen7@outlook.com>
2024-10-07 07:49:37 +0000

    Increase lr

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
+python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
+python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
+python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
+python -m scripts.benchmarks.hyp_smc_benchmark --q-matrix jc69
