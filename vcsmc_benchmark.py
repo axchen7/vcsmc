3a93e4572f04a56789bac0c3ddf96b25cfa0d3dc
Alex Chen <axchen7@outlook.com>
2024-12-02 03:29:26 +0000

    Add vcsmc benchmark

---
+++ vcsmc_benchmark.py
@@ -0,0 +1,13 @@
+from scripts.train.hyp_train import QMatrixType, hyp_train
+
+for dataset in ["DS1", "DS2", "DS3", "DS4", "DS5", "DS6", "DS8"]:
+    print(f"Running {dataset}...")
+    hyp_train(
+        lr=0.1,
+        epochs=200,
+        K=512,
+        q_matrix=QMatrixType.STATIONARY,
+        hyperbolic=False,
+        file=f"data/hohna/{dataset}.phy",
+        run_name=f"VCSMC-{dataset}",
+    )
