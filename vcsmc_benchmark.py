7fc080869d10a86ed7b2c06cc639f9318d012482
Alex Chen <axchen7@outlook.com>
2024-12-02 15:24:52 +0000

    Checkpoint grads

--- vcsmc_benchmark.py
+++ vcsmc_benchmark.py
@@ -10,6 +10,7 @@ for dataset in ["DS1", "DS2", "DS3", "DS4", "DS5", "DS6", "DS8"]:
                 K=K,
                 q_matrix=QMatrixType.STATIONARY,
                 hyperbolic=False,
+                checkpoint_grads=True,
                 file=f"data/hohna/{dataset}.phy",
                 run_name=f"VCSMC-{dataset}-K{K}",
             )
